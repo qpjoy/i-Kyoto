b6960bc2da12f4d1346b417ff23e51308da65672
qpjoy <15150121121@126.com>
2025-06-19 23:17:13 +0800

    feat: nodemailer test

--- tech/de/linux/git/rmSensitiveData.sh
+++ tech/de/linux/git/rmSensitiveData.sh
@@ -1,1 +1,10 @@
-git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch tech/de/docker/nginx/nginx.services.conf' --prune-empty --tag-name-filter cat -- --all
+git filter-branch --force --index-filter \
+  'git rm --cached --ignore-unmatch tech/de/docker/nginx/nginx.services.conf' \
+  --prune-empty --tag-name-filter cat -- --all
+
+rm -rf .git/refs/original/
+git reflog expire --expire=now --all
+git gc --prune=now --aggressive
+
+git push --force --all
+git push --force --tags
